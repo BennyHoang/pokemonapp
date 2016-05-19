3293a234de83c1fa9800475cf00241ab67925b19
Ole Fredrik Schreuder <olifredrik@gmail.com>
2016-05-19 14:28:17 +0200

    added coded buttons

--- app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MainActivity.java
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MainActivity.java
@@ -1,13 +1,50 @@
 package no.westerdals.student.gruppe6.pokemonapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnYourPokemons;
+    private Button btnCatchPokemons;
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+
+        btnCatchPokemons = (Button) findViewById(R.id.btnCatchPokemon);
+        btnYourPokemons = (Button) findViewById(R.id.btnYourPokemons);
+
+        onClickBtnYourPokemons();
+    }
+
+    //TODO: Replace activity class with something interesting
+    private void onClickBtnYourPokemons(){
+        btnYourPokemons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PokeMapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onClickBtnCatchPokemons(){
+        btnCatchPokemons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PokeMapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
