5267115d251b363fd7f14e7faff5a8fbc100a568
Benny <hoaben13@student.westerdals.no>
2016-05-21 18:11:49 +0200

    enabled authorization with tokens too Web-Api

--- app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/CatchPokemonActivity.java
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/CatchPokemonActivity.java
@@ -1,13 +1,75 @@
 package no.westerdals.student.gruppe6.pokemonapp;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
 
 public class CatchPokemonActivity extends AppCompatActivity {
+    private Button btnSubmitId;
+    private TextView responseTextView;
+    String apiUrl = "https://locations.lehmann.tech/pokemon/5735a3f86d4c35dab3946e08";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catch_pokemon);
+
+        btnSubmitId = (Button) findViewById(R.id.btnSubmitId);
+        responseTextView = (TextView) findViewById(R.id.responseTextView);
+
+        btnSubmitId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAndDisplayData();
+            }
+
+        });
     }
+
+    void getAndDisplayData() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(final Void... params) {
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
+                    connection.setRequestProperty("X-Token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ImdydXBwZSA2Ig.ZWVrv8AWDiH_X358jZ6IYrNgMXDq1B7UvbyiDoEP2Q0");
+
+                    try {
+                        InputStream inputStream = connection.getInputStream();
+                        Scanner scanner = new Scanner(inputStream);
+
+                        StringBuilder stringBuilder = new StringBuilder();
+                        while (scanner.hasNextLine()) {
+                            stringBuilder.append(scanner.nextLine());
+                        }
+
+                        return stringBuilder.toString();
+                    } catch (FileNotFoundException e) {
+                        return "GOT ERROR WITH CODE: " + connection.getResponseCode() + " with message: " + connection.getResponseMessage();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            protected void onPostExecute(final String response) {
+                super.onPostExecute(response);
+                responseTextView.setText(response);
+            }
+        }.execute();
+    }
+
 }
