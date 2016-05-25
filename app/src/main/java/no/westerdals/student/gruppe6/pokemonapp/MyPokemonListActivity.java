78dd86059f8dcf0630a4eca63578d1a307d6a438
Benny <hoaben13@student.westerdals.no>
2016-05-25 16:14:48 +0200

    added popup functionality

--- app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
@@ -1,9 +1,17 @@
 package no.westerdals.student.gruppe6.pokemonapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import java.util.ArrayList;
 
@@ -22,12 +30,23 @@ public class MyPokemonListActivity extends AppCompatActivity {
         lv.setAdapter(adapter);
     }
 
-    private ArrayList<String> parsePokemon(ArrayList<MyPokemon> list){
+    private ArrayList<String> parsePokemon(ArrayList<MyPokemon> list) {
         ArrayList<String> tempList = new ArrayList<>();
         for (MyPokemon p : list) {
             tempList.add(p.getName() + " " + p.getPokemonID());
         }
         return tempList;
+    }
+
+    private PopupWindow pwindow;
+
+    private void showPopup() {
+        LayoutInflater inflater = (LayoutInflater) MyPokemonListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.screen_popup, (ViewGroup) findViewById(R.id.popup_element));
+        pwindow = new PopupWindow(layout, 300, 370, true);
+        pwindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+
     }
 
 }
