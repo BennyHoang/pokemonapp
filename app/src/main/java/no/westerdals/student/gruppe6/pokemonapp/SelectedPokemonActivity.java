e3bd360e852f0b64ead459610bfe02b26f20bd83
Benny <hoaben13@student.westerdals.no>
2016-05-25 21:11:07 +0200

    added image and layout for this pokemon

--- app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/SelectedPokemonActivity.java
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/SelectedPokemonActivity.java
@@ -1,22 +1,57 @@
 package no.westerdals.student.gruppe6.pokemonapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
+import java.net.URL;
+
 public class SelectedPokemonActivity extends AppCompatActivity {
-    private TextView pokemon_id;
+    private TextView pokemon_name;
+    private MyPokemon myPokemon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_pokemon);
         Intent i = getIntent();
         String id = i.getExtras().getString("Pokemon");
-        pokemon_id = (TextView) findViewById(R.id.pokemon_id);
+        myPokemon = new MyPokemonManager(getApplicationContext()).getPokemonById(id);
+        pokemon_name = (TextView) findViewById(R.id.pokemon_name);
+        pokemon_name.setText(myPokemon.getName());
+        new DownloadImageTask((ImageView) findViewById(R.id.pokemon_image)).execute(myPokemon.getImageURL());
+    }
 
-        pokemon_id.setText(id);
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
