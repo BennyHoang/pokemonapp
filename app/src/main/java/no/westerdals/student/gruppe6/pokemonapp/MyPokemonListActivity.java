a250582683f4d9fb23c3ed57d8d254d8ac354a73
Benny <hoaben13@student.westerdals.no>
2016-05-25 17:58:59 +0200

    listview opens up selected pokemon

--- app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
@@ -3,6 +3,9 @@ package no.westerdals.student.gruppe6.pokemonapp;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -11,10 +14,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MyPokemonListActivity extends AppCompatActivity {
@@ -32,12 +38,19 @@ public class MyPokemonListActivity extends AppCompatActivity {
         final ListView lv = (ListView) findViewById(R.id.listView);
         lv.setAdapter(adapter);
 
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedFromList =(String) (lv.getItemAtPosition(position));
-                showPopup(selectedFromList);
+                String selectedFromList = (String) (lv.getItemAtPosition(position));
+                final ArrayList<MyPokemon> myPokemons = new MyPokemonManager(getApplicationContext()).getMyPokemonList();
+                for(MyPokemon p : myPokemons){
+                    if(selectedFromList.equals(p.getName())){
+                        showPopup(p.getName(), p.getImageURL());
+                    }
+                }
+
             }
         });
     }
@@ -45,21 +58,24 @@ public class MyPokemonListActivity extends AppCompatActivity {
     private ArrayList<String> parsePokemon(ArrayList<MyPokemon> list) {
         ArrayList<String> tempList = new ArrayList<>();
         for (MyPokemon p : list) {
-            tempList.add(p.getName() + " " + p.getPokemonID());
+            tempList.add(p.getName());
         }
         return tempList;
     }
 
     private PopupWindow pwindow;
 
-    private void showPopup(String name) {
+    private void showPopup(String name, String image) {
         LayoutInflater inflater = (LayoutInflater) MyPokemonListActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View layout = inflater.inflate(R.layout.screen_popup, (ViewGroup) findViewById(R.id.popup_element));
-        pwindow = new PopupWindow(layout, 300, 370, true);
+        pwindow = new PopupWindow(layout, 700, 370, true);
         pwindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
-        TextView pokemon_title = (TextView) layout.findViewById(R.id.pokemon_title);
-        pokemon_title.setText(name);
+        TextView pokemon_name = (TextView) layout.findViewById(R.id.pokemon_name);
+        ImageView pokemon_image = (ImageView) layout.findViewById(R.id.pokemon_image);
+        pokemon_name.setText(name);
+        pokemon_image.setImageURI(Uri.parse(image));
 
     }
+
 
 }
