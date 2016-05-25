0d617ddee5ef44c14de12ded0044e8f8a50419bd
Benny <hoaben13@student.westerdals.no>
2016-05-25 18:08:05 +0200

    created new activity for this pokemon

--- app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
@@ -2,6 +2,7 @@ package no.westerdals.student.gruppe6.pokemonapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
@@ -47,10 +48,21 @@ public class MyPokemonListActivity extends AppCompatActivity {
                 final ArrayList<MyPokemon> myPokemons = new MyPokemonManager(getApplicationContext()).getMyPokemonList();
                 for(MyPokemon p : myPokemons){
                     if(selectedFromList.equals(p.getName())){
-                        showPopup(p.getName(), p.getImageURL());
+                        Intent intent = new Intent(MyPokemonListActivity.this, SelectedPokemonActivity.class);
+                        intent.putExtra("Pokemon", myPokemons);
+                        startActivity(intent);
                     }
                 }
 
+                /*
+                String selectedFromList = (String) (lv.getItemAtPosition(position));
+                final ArrayList<MyPokemon> myPokemons = new MyPokemonManager(getApplicationContext()).getMyPokemonList();
+                for(MyPokemon p : myPokemons){
+                    if(selectedFromList.equals(p.getName())){
+                        showPopup(p.getName(), p.getImageURL());
+                    }
+                }
+                */
             }
         });
     }
