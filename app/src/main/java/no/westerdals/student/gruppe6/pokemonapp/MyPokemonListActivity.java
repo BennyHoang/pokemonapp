f0af8831678db2f26540aec5de27d882cfb30df3
Ole Fredrik Schreuder <olifredrik@gmail.com>
2016-05-25 14:03:21 +0200

    added MyPokemonListActivity

---
+++ app/src/main/java/no/westerdals/student/gruppe6/pokemonapp/MyPokemonListActivity.java
@@ -0,0 +1,13 @@
+package no.westerdals.student.gruppe6.pokemonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPokemonListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_pokemon_list);
+    }
+}
